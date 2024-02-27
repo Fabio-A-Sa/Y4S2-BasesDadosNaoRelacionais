680c84e82624c80d80d490eb4d764626b5953e96
Fábio Sá <up202007658@edu.fe.up.pt>
2024-02-27 17:51:36 +0000

    add stuff

--- Labs/1 - Redis/start.sh
+++ Labs/1 - Redis/start.sh
@@ -5,11 +5,11 @@ docker rm lab1
 # Run
 docker run -d --name lab1 -v ./redis-data:/data -p 6379:6379 redis
 
-# Interactive CLI
-# docker run -it --rm --network container:lab1 redis redis-cli
-
 # Kill old port
 kill -9 $(lsof -ti:8080)
 
 # Run web server
 php -S localhost:8080
+
+# Interactive CLI - Monitoring
+# docker run -it --rm --network container:lab1 redis redis-cli
