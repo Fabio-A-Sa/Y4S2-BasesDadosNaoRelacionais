21b28abbd168819d944c1a509c87c755f5833dad
Fábio Sá <up202007658@edu.fe.up.pt>
2024-02-27 18:44:18 +0000

    lab 1 done, +/-

--- Labs/1 - Redis/index.php
+++ Labs/1 - Redis/index.php
@@ -5,75 +5,58 @@
 
 <?php
 
-function present_bookmarks($bookmarks, $redis) {
-    echo "<ul>";
-        foreach ($bookmarks as $bookmark) {
-            $url = $redis->hget("bookmark:" . $bookmark, "url");
-            $bookmark_tags = $redis->smembers("bookmark:" . $bookmark . ":tags");
-            ?>
-
-            <li>
-                <h4><?=$url?></h4>
-                <ul>
-                    <?php
-                        foreach($bookmark_tags as $tag) {
-                         ?>
-                         <li><?=$tag?></li>
-                         <?php
-                        }
-                    ?>
-                </ul>
-            </li>
-
-            <?php
-        }
-    echo "</ul>";
-}
-
-function prepend_tag($tag) {
-    return "tag:".$tag;
-}
-
 require __DIR__ . '/vendor/autoload.php';
 
 Predis\Autoloader::register();
 
+function showBookmarks($bookmarks, $redis) {
+    echo "<ul>";
+        foreach ($bookmarks as $bookmark) {
+            $url = $redis->hget("bookmark:" . $bookmark, "url");
+            $tags = $redis->smembers("bookmark:" . $bookmark . ":tags");
+        ?>
+        
+        <li>
+            <h4><?= $url ?></h4>
+            <h5>[
+            <?php foreach($tags as $tag) {
+                echo $tag;
+            }; ?>
+            ]</h5>
+        </li>
+
+    <?php } echo "</ul>";
+}
+
 try {
 
     $redis = new Predis\Client();
+    $bookmarks = array();
 
+    // Next ID
     if (!$redis->exists("next_bookmark_id")) {
         $redis->set("next_bookmark_id", "0");
     }
 
+    // With parameters -> show
     if (isset($_GET["tags"])) {
         $tags = explode(",", $_GET["tags"]);
-    
-        
-        if (sizeof($tags) === 1)
-        {
-            $bookmarks = $redis->smembers("tag:" . $_GET["tags"]);
-            
-        } 
-        else {
-       
-            $bookmarks = $redis->sinter(array_map("prepend_tag",$tags));
-           
+        foreach ($tags as $tag) {
+            $bookmarks[] =  "tag:".$tag;
         }
 
-        present_bookmarks($bookmarks, $redis);
+        $bookmarks = $redis->sinter($bookmarks);
+    
+    // Without parameters -> show last 15, if available
+    } else {
+        $bookmarks = $redis->zrange("bookmarks", -15, -1);
     }
-    else {
-        $last_15_bookmarks = $redis->zrange("bookmarks", -15, -1);
-        present_bookmarks($last_15_bookmarks, $redis);
-    }
+
+    showBookmarks($bookmarks, $redis);
 
 } catch (Exception $e) {
     print $e->getMessage();
-}
-
-
-?>
+} ?>
 
 <a href="index.php">Home</a>
 <a href="add.html">Add another bookmark!</a>
