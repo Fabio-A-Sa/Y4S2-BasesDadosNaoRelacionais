9911157f0723f87dfe239a0a6a2bdf2f251e85f4
Fábio Sá <up202007658@edu.fe.up.pt>
2024-03-10 20:10:32 +0000

    drafts

--- Labs/2 - Mongo/test.php
+++ Labs/2 - Mongo/test.php
@@ -3,35 +3,35 @@
 require __DIR__ . '/vendor/autoload.php';
 
 try {
-        // Connect to the local Mongo server.
-        $mongo = new MongoDB\Client();
+    // Connect to the local Mongo server.
+    $mongo = new MongoDB\Client();
 
-        // Select a database (create if it doesn't exist).
-        $db = $mongo->selectDatabase('bdnr-php');
+    // Select a database (create if it doesn't exist).
+    $db = $mongo->selectDatabase('bdnr-php');
 
-        // Select a collection.
-        $docs = $db->docs;
+    // Select a collection.
+    $docs = $db->docs;
 
-        // Create two new documents.
-        $new_docs = [
+    // Create two new documents.
+    $new_docs = [
         [
-           'country' => 'France',
-           'capital' => 'Paris'
+            'country' => 'France',
+            'capital' => 'Paris'
         ],
         [
-           'country' => 'Italy',
-           'capital' => 'Rome'
+            'country' => 'Italy',
+            'capital' => 'Rome'
         ]
-        ];
+    ];
 
-        // Insert documents to collection.
-        $insert = $docs->insertMany($new_docs);
+    // Insert documents to collection.
+    $insert = $docs->insertMany($new_docs);
 
-        // Print insert information.
-        print_r($insert);
+    // Print insert information.
+    print_r($insert);
 
 } catch (Exception $e) {
-        print $e->getMessage();
+    print $e->getMessage();
 };
 
 ?>
